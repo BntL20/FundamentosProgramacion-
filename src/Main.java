d3eda997d686884e3b07339d90729ce7c06f0968
sofia <peralcolladosofia@gmail.com>
2025-12-11 20:38:04 +0100

    PRUEBA 1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Hola");
+        System.out.println("HOLA");
+
     }
 }
