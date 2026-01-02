7010cc86ef3c6d7ca5f07caa43e97ac2e45bc7fb
danna <dannagz29@gmail.com>
2026-01-01 23:05:38 -0500

    parte del usuario danna(no se si esta bien)

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class Usuario {
-    String nombre;
-    int edad;
-    String correo;
+    public String nombre;
+    public int edad;
+    public String correo;
 
     public Usuario(String nombre, int edad, String correo) {
         this.nombre = nombre;
@@ -9,4 +10,43 @@ public class Usuario {
         this.correo = correo;
     }
 
-}
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public static boolean validarEdad(int edad){
+        return edad >=0;
+    }
+    public static boolean validarCorreo(String correo){
+        return correo.contains("@")&& correo.contains(".");
+    }
+    public static Usuario registrarUsuario(){
+        Scanner in = new Scanner(System.in);
+        String nombre;
+        int edad;
+        String correo;
+
+      System.out.println("Ingrese su nombre:");
+      nombre= in.nextLine();
+      do {
+          System.out.println("Ingrese su edad");
+          edad = in.nextInt();
+      } while (!validarEdad(edad));
+      in.nextLine();
+
+      do {
+          System.out.println("Ingrese su correo: ");
+          correo = in.nextLine();
+      } while(!validarCorreo(correo));
+      return new Usuario(nombre,edad, correo);
+      }
+    }
+
+
